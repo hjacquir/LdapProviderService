7231822d27b20d99075473aaf8c7be44f1aa6477
hatim JACQUIR <jacquirhatim@gmail.com>
2014-08-23 00:28:22 +0000

    fix : add updated tests

--- tests/Hj/Unit/ConnectionTest.php
+++ tests/Hj/Unit/ConnectionTest.php
@@ -7,8 +7,67 @@
 
 namespace Tests\Unit;
 
+use Hj\Connection;
 
-class ConnectionTest
+/**
+ * Class ConnectionTest
+ * @package Tests\Unit
+ *
+ * @covers Hj\Connection
+ */
+class ConnectionTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Connection
+     */
+    private $sut;
 
-} 
+    public function setUp()
+    {
+        $this->sut = new Connection();
+    }
+
+    public function testShouldHaveA389PortByDefault()
+    {
+        $this->assertAttributeEquals(389, 'port', $this->sut);
+    }
+
+    public function testShouldHaveAHostnameAsNullByDefault()
+    {
+        $this->assertAttributeEquals(null, 'hostname', $this->sut);
+    }
+
+    public function testShouldOverrideDefaultValues()
+    {
+        $sut = new Connection(14, 'foo');
+
+        $this->assertSame(14, $sut->getPort());
+        $this->assertSame('foo', $sut->getHostname());
+    }
+
+    /**
+     * @dataProvider providePortData
+     *
+     * @param integer $port
+     *
+     * @expectedException \Hj\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The given value
+     */
+    public function testShouldThrowAnExceptionWithInvalidPort($port)
+    {
+        new Connection($port);
+    }
+
+    /**
+     * @return array
+     */
+    public function providePortData()
+    {
+        return array(
+            array('foo'),
+            array(null),
+            array(''),
+            array(1.58),
+        );
+    }
+}
--- tests/Hj/Unit/GuardianTest.php
+++ tests/Hj/Unit/GuardianTest.php
@@ -5,26 +5,25 @@
  * @author : Hatim Jacquir <jacquirhatim@gmail.com>
  */
 
-namespace Tests;
+namespace Tests\Unit;
 
 use Hj\Guardian;
 
+/**
+ * Class GuardianTest
+ * @package Tests\Unit
+ *
+ * @covers Hj\Guardian
+ */
 class GuardianTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var Guardian
-     */
-    private $sut;
-
-    public function setUp()
-    {
-
-    }
-
     /**
      * @dataProvider provideInvalidIntegerData
      *
      * @param mixed $value
+     *
+     * @expectedException \Hj\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The given value must be an integer
      */
     public function testGuardAgainstInvalidInteger($value)
     {
@@ -41,7 +40,8 @@ class GuardianTest extends \PHPUnit_Framework_TestCase
             array(null),
             array('foo'),
             array(1.2),
+            array(array()),
+            array(new \stdClass()),
         );
     }
-
 }
